8b41e0e670852313ec69984aded658b132853916
Thomas ten Cate <ttencate@gmail.com>
2015-08-23 16:38:56 +0200

    Fix upload

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rltv --delete --exclude-from='no-upload.txt' ./ frozenfractal.com:/var/www/glauron.frozenfractal.com/
+rsync -rpltv --delete --exclude-from='no-upload.txt' ./ frozenfractal.com:/var/www/glauron.frozenfractal.com/
